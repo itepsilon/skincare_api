0ea1795d10da5d81a5b4babf645b51e45538f40f
Ganbold Li <ganbold.li@gmail.com>
2020-03-28 11:21:36 -0700

    added tag model

--- skincare_app/models.py
+++ skincare_app/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=30, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+
 class Product(models.Model):
     name = models.CharField(max_length=30, blank=True, null=True)
     description = models.CharField(max_length=500, blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
     image_url = models.CharField(max_length=500, blank=True, null=True)
+    tags = models.ManyToManyField(Tag, related_name='products')
