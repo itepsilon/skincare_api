7d6025444d56f4e240c4b15eccab351fdd3c4854
Ganbold Li <ganbold.li@gmail.com>
2020-03-28 11:39:18 -0700

    added tag model

--- skincare_app/admin.py
+++ skincare_app/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from skincare_app.models import Product
+from skincare_app.models import Product, Tag
 
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_filter = ('name', 'description', 'price')
+    list_filter = ('name', 'description', 'price', 'tags')
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_filter = ('name', 'description')
