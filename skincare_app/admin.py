e8b1f7274b78a28e21c00b9c0adac1cc0980fcce
Ganbold Li <ganbold.li@gmail.com>
2020-04-04 11:58:48 -0700

    added profile

--- skincare_app/admin.py
+++ skincare_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from skincare_app.models import Product, Tag
+from skincare_app.models import Product, Tag, Profile
 
 # Register your models here.
 @admin.register(Product)
@@ -9,3 +9,7 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_filter = ('name', 'description')
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_filter = ('user', 'first_name', 'last_name', 'middle_name', 'favorite_products')
--- skincare_app/models.py
+++ skincare_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Tag(models.Model):
     name = models.CharField(max_length=30, blank=True, null=True)
@@ -16,3 +17,10 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
+    favorite_products = models.ManyToManyField(Product, related_name='liked_by', null=True, blank=True)
