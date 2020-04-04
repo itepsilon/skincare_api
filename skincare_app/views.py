3b6158c5bb147343836f2cd7e3f6899bb222baac
Ganbold Li <ganbold.li@gmail.com>
2020-04-03 20:48:55 -0700

    api

--- skincare_app/views.py
+++ skincare_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 from skincare_app.models import Product
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class ListProducts(APIView):
+    """
+    View to list all products.
+    """
+
+    def get(self, request):
+        """
+        Return a list of all products.
+        """
+        products = Product.objects.all()
+        return Response(products)
