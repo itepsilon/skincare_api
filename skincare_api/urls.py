cee48bb848b2338e6a5123de63e1f495bf1d82cc
Ganbold Li <ganbold.li@gmail.com>
2020-04-04 06:48:05 -0700

    product api

--- skincare_api/urls.py
+++ skincare_api/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework_simplejwt import views as jwt_views
+from skincare_app.views import product_collection
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('^api-auth/', include('rest_framework.urls')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/products/$', name="product_collection")
+    path('api/products/$', product_collection, name="product_collection")
 ]
