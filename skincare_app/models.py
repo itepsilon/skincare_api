af650679702d42719b1c8cc640d5fd7b47952fad
Ganbold Li <ganbold.li@gmail.com>
2020-04-03 19:50:01 -0700

    tag name

--- skincare_app/models.py
+++ skincare_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=30, blank=True, null=True)
     description = models.CharField(max_length=500, blank=True, null=True)
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=30, blank=True, null=True)
