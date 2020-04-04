38052348cfd1f14943c786b2c054bd513ee75cce
Ganbold Li <ganbold.li@gmail.com>
2020-04-04 12:07:06 -0700

    done

--- skincare_app/models.py
+++ skincare_app/models.py
@@ -24,3 +24,6 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=50, blank=True, null=True)
     middle_name = models.CharField(max_length=50, blank=True, null=True)
     favorite_products = models.ManyToManyField(Product, related_name='liked_by', null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name + self.last_name
